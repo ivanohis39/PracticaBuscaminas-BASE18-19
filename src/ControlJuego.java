ff09133db4864f9c862c7347669f209e4359a348
Ivan Hisado <ivanhisadotejeda@gmail.com>
2018-10-26 15:11:43 +0200

    Nuevas modificaciones

--- src/ControlJuego.java
+++ src/ControlJuego.java
@@ -42,10 +42,6 @@ public class ControlJuego {
 	 *        minas guardan en el entero cu√°ntas minas hay alrededor de la celda
 	 */
 
-	public void metodoPrueba() {
-
-	}
-
 	public void inicializarPartida() {
 
 		// INTRODUCIMOS UN NUMERO EN EJE DE LAS X y EN DE LAS Y
@@ -76,7 +72,7 @@ public class ControlJuego {
 				if (tablero[i][j] != MINA) {
 					// SI LA CASILLA NO ES UNA MINA LE A—ADIMOS EL VALOR DE LAS MINAS
 					// QUE TIENE ALREDEDOR
-					tablero[i][j] = calculoMinasAdjuntas(i, j); 
+					tablero[i][j] = calculoMinasAdjuntas(i, j);
 				}
 			}
 		}
@@ -85,18 +81,20 @@ public class ControlJuego {
 	/**
 	 * Calculo de las minas adjuntas: Para calcular el numero de minas tenemos que
 	 * tener en cuenta que no nos salimos nunca del tablero. Por lo tanto, como
-	 * mucho la i y la j valdran LADO_TABLERO-1. Por lo tanto, como poco la i y la
-	 * j valdran 0.
+	 * mucho la i y la j valdran LADO_TABLERO-1. Por lo tanto, como poco la i y la j
+	 * valdran 0.
 	 * 
-	 * @param i: posicion vertical de la casilla a rellenar
-	 * @param j: posicion horizontal de la casilla a rellenar
+	 * @param i:
+	 *            posicion vertical de la casilla a rellenar
+	 * @param j:
+	 *            posicion horizontal de la casilla a rellenar
 	 * @return : El numero de minas que hay alrededor de la casilla [i][j]
 	 **/
 	private int calculoMinasAdjuntas(int i, int j) {
 		int cuentaMinas = 0; // INICIAMOS EL CONTADOR DE LAS MINAS
-		for (i = 0; i < tablero.length; i++) {
-			for (j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] == MINA) {
+		for (i = 0; i < tablero.length-1; i++) {
+			for (j = 0; j < tablero[i].length-1; j++) {
+				if (tablero[i][j] == MINA) {
 					cuentaMinas++;
 				}
 			}
@@ -109,19 +107,20 @@ public class ControlJuego {
 	 * 
 	 * @pre : La casilla nunca debe haber sido abierta antes, no es controlado por
 	 *      el ControlJuego. Por lo tanto siempre sumaremos puntos
-	 * @param i: posicion verticalmente de la casilla a abrir
-	 * @param j: posicion horizontalmente de la casilla a abrir
+	 * @param i:
+	 *            posicion verticalmente de la casilla a abrir
+	 * @param j:
+	 *            posicion horizontalmente de la casilla a abrir
 	 * @return : Verdadero si no ha explotado una mina. Falso en caso contrario.
 	 */
 	public boolean abrirCasilla(int i, int j) {
-		boolean noExplota = false;
-		for (i = 0;i  < tablero.length; i++) {
-			for ( j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] != MINA) {
-					noExplota = true;
-				}
-			}
+		boolean noExplota = true;
+		// SI LA CASILLA ABIERTA ES UNA MINA ENVIA FALSO Y POR LO TANTO EXPLOTA
+		// MIENTRAS NO SEA UNA MINA NOS ENVIARA TRUE Y POR LO TANTO NO EXLOTARA
+		if (tablero[i][j] == MINA) {
+			noExplota = false;
 		}
+
 		return noExplota;
 	}
 
@@ -135,14 +134,15 @@ public class ControlJuego {
 	public boolean esFinJuego() {
 		boolean esFIN = false;
 		int cuentaAcierto = 0;
+		
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] == MINA) {
-					cuentaAcierto++; 
+				if (tablero[i][j] != MINA) {
+					cuentaAcierto++;
 				}
 			}
 		}
-		if(cuentaAcierto == 80) {
+		if (cuentaAcierto == 80) {
 			esFIN = true;
 		}
 		return esFIN;
@@ -168,13 +168,15 @@ public class ControlJuego {
 	 * 
 	 * @pre : El tablero tiene que estar ya inicializado, por lo tanto no hace falta
 	 *      calcularlo, simplemente consultarlo
-	 * @param i : posicion vertical de la celda.
-	 * @param j : posicion horizontal de la cela.
+	 * @param i
+	 *            : posicion vertical de la celda.
+	 * @param j
+	 *            : posicion horizontal de la cela.
 	 * @return Un entero que representa el numero de minas alrededor de la celda
 	 */
 	public int getMinasAlrededor(int i, int j) {
 		int cuentaMinas = 0;
-		if(tablero[i][j] == MINA) {
+		if (tablero[i][j] == MINA) {
 			cuentaMinas++;
 		}
 		return cuentaMinas;
@@ -189,7 +191,7 @@ public class ControlJuego {
 		puntuacion = 0;
 		for (int i = 0; i < tablero.length; i++) {
 			for (int j = 0; j < tablero[i].length; j++) {
-				if(tablero[i][j] != MINA) {
+				if (tablero[i][j] != MINA) {
 					puntuacion++;
 				}
 			}
--- src/VentanaPrincipal.java
+++ src/VentanaPrincipal.java
@@ -4,6 +4,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.PasswordAuthentication;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,70 +15,66 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-
 public class VentanaPrincipal {
 
-	//La ventana principal, en este caso, guarda todos los componentes:
+	// La ventana principal, en este caso, guarda todos los componentes:
 	JFrame ventana;
 	JPanel panelImagen;
 	JPanel panelEmpezar;
 	JPanel panelPuntuacion;
 	JPanel panelJuego;
-	
-	//Todos los botones se meten en un panel independiente.
-	//Hacemos esto para que podamos cambiar después los componentes por otros
-	JPanel [][] panelesJuego;
-	JButton [][] botonesJuego;
-	
-	//Correspondencia de colores para las minas:
-	Color correspondenciaColores [] = {Color.BLACK, Color.CYAN, Color.GREEN, Color.ORANGE, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED};
-	
+
+	// Todos los botones se meten en un panel independiente.
+	// Hacemos esto para que podamos cambiar después los componentes por otros
+	JPanel[][] panelesJuego;
+	JButton[][] botonesJuego;
+
+	// Correspondencia de colores para las minas:
+	Color correspondenciaColores[] = { Color.BLACK, Color.CYAN, Color.GREEN, Color.ORANGE, Color.RED, Color.RED,
+			Color.RED, Color.RED, Color.RED, Color.RED };
+
 	JButton botonEmpezar;
 	JTextField pantallaPuntuacion;
-	
-	
-	//LA VENTANA GUARDA UN CONTROL DE JUEGO:
+
+	// LA VENTANA GUARDA UN CONTROL DE JUEGO:
 	ControlJuego juego;
-	
-	
-	//Constructor, marca el tamaño y el cierre del frame
+
+	// Constructor, marca el tamaño y el cierre del frame
 	public VentanaPrincipal() {
 		ventana = new JFrame();
 		ventana.setBounds(100, 100, 700, 500);
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		juego = new ControlJuego();
 	}
-	
-	//Inicializa todos los componentes del frame
-	public void inicializarComponentes(){
-		
-		//Definimos el layout:
+
+	// Inicializa todos los componentes del frame
+	public void inicializarComponentes() {
+
+		// Definimos el layout:
 		ventana.setLayout(new GridBagLayout());
-		
-		//Inicializamos componentes
+
+		// Inicializamos componentes
 		panelImagen = new JPanel();
 		panelEmpezar = new JPanel();
-		panelEmpezar.setLayout(new GridLayout(1,1));
+		panelEmpezar.setLayout(new GridLayout(1, 1));
 		panelPuntuacion = new JPanel();
-		panelPuntuacion.setLayout(new GridLayout(1,1));
+		panelPuntuacion.setLayout(new GridLayout(1, 1));
 		panelJuego = new JPanel();
-		panelJuego.setLayout(new GridLayout(10,10));
-		
-		
+		panelJuego.setLayout(new GridLayout(10, 10));
+
 		botonEmpezar = new JButton("Go!");
 		pantallaPuntuacion = new JTextField("0");
 		pantallaPuntuacion.setEditable(false);
 		pantallaPuntuacion.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		//Bordes y colores:
+
+		// Bordes y colores:
 		panelImagen.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 		panelEmpezar.setBorder(BorderFactory.createTitledBorder("Empezar"));
 		panelPuntuacion.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 		panelJuego.setBorder(BorderFactory.createTitledBorder("Juego"));
-		
-			
-		//Colocamos los componentes:
-		//AZUL
+
+		// Colocamos los componentes:
+		// AZUL
 		GridBagConstraints settings = new GridBagConstraints();
 		settings.gridx = 0;
 		settings.gridy = 0;
@@ -84,7 +82,7 @@ public class VentanaPrincipal {
 		settings.weighty = 1;
 		settings.fill = GridBagConstraints.BOTH;
 		ventana.add(panelImagen, settings);
-		//VERDE
+		// VERDE
 		settings = new GridBagConstraints();
 		settings.gridx = 1;
 		settings.gridy = 0;
@@ -92,7 +90,7 @@ public class VentanaPrincipal {
 		settings.weighty = 1;
 		settings.fill = GridBagConstraints.BOTH;
 		ventana.add(panelEmpezar, settings);
-		//AMARILLO
+		// AMARILLO
 		settings = new GridBagConstraints();
 		settings.gridx = 2;
 		settings.gridy = 0;
@@ -100,7 +98,7 @@ public class VentanaPrincipal {
 		settings.weighty = 1;
 		settings.fill = GridBagConstraints.BOTH;
 		ventana.add(panelPuntuacion, settings);
-		//ROJO
+		// ROJO
 		settings = new GridBagConstraints();
 		settings.gridx = 0;
 		settings.gridy = 1;
@@ -109,18 +107,18 @@ public class VentanaPrincipal {
 		settings.gridwidth = 3;
 		settings.fill = GridBagConstraints.BOTH;
 		ventana.add(panelJuego, settings);
-		
-		//Paneles
+
+		// Paneles
 		panelesJuego = new JPanel[10][10];
 		for (int i = 0; i < panelesJuego.length; i++) {
 			for (int j = 0; j < panelesJuego[i].length; j++) {
 				panelesJuego[i][j] = new JPanel();
-				panelesJuego[i][j].setLayout(new GridLayout(1,1));
+				panelesJuego[i][j].setLayout(new GridLayout(1, 1));
 				panelJuego.add(panelesJuego[i][j]);
 			}
 		}
-		
-		//Botones
+
+		// Botones
 		botonesJuego = new JButton[10][10];
 		for (int i = 0; i < botonesJuego.length; i++) {
 			for (int j = 0; j < botonesJuego[i].length; j++) {
@@ -128,64 +126,94 @@ public class VentanaPrincipal {
 				panelesJuego[i][j].add(botonesJuego[i][j]);
 			}
 		}
-		
-		//BotónEmpezar:
+
+		// BotonEmpezar:
 		panelEmpezar.add(botonEmpezar);
 		panelPuntuacion.add(pantallaPuntuacion);
-		
-	}
-	
-	/**
-	 * Método que inicializa todos los lísteners que necesita inicialmente el programa
-	 */
-	public void inicializarListeners(){
-		//TODO
-	}
-	
-	
-	/**
-	 * Pinta en la pantalla el número de minas que hay alrededor de la celda
-	 * Saca el botón que haya en la celda determinada y añade un JLabel centrado y no editable con el número de minas alrededor.
-	 * Se pinta el color del texto según la siguiente correspondecia (consultar la variable correspondeciaColor):
-	 * - 0 : negro
-	 * - 1 : cyan
-	 * - 2 : verde
-	 * - 3 : naranja
-	 * - 4 ó más : rojo 
-	 * @param i: posición vertical de la celda.
-	 * @param j: posición horizontal de la celda.
-	 */
-	public void mostrarNumMinasAlrededor(int i , int j) {
-		//TODO
-	}
-	
-	
-	/**
-	 * Muestra una ventana que indica el fin del juego
-	 * @param porExplosion : Un booleano que indica si es final del juego porque ha explotado una mina (true) o bien porque hemos desactivado todas (false) 
-	 * @post : Todos los botones se desactivan excepto el de volver a iniciar el juego.
-	 */
-	public void mostrarFinJuego(boolean porExplosion) {
-		//TODO
+
 	}
 
 	/**
-	 * Método que muestra la puntuación por pantalla.
+	 * Metodo que inicializa todos los listeners que necesita inicialmente el
+	 * programa
+	 */int i = 0;
+		int j = 0;
+	public void inicializarListeners() {
+		
+		for (i = 0; i < botonesJuego.length; i++) {
+			int iInterna;
+		
+			for (j = 0; j < botonesJuego[i].length; j++) {
+			
+				botonesJuego[i][j].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						mostrarNumMinasAlrededor(i, j);
+
+					}
+				});
+			}
+
+		}
+	}
+
+	/**
+	 * Pinta en la pantalla el numero de minas que hay alrededor de la celda Saca el
+	 * boton que haya en la celda determinada y a�ade un JLabel centrado y no
+	 * editable con el numero de minas alrededor. Se pinta el color del texto segun
+	 * la siguiente correspondecia (consultar la variable correspondeciaColor): - 0
+	 * : negro - 1 : cyan - 2 : verde - 3 : naranja - 4 ó más : rojo
+	 * 
+	 * @param i: posicion vertical de la celda.
+	 * @param j: posicion horizontal de la celda.
+	 */
+	public void mostrarNumMinasAlrededor(int i, int j) {
+		if (juego.abrirCasilla(i, j)) {
+			String minas = String.valueOf(juego.getMinasAlrededor(i, j));
+			JLabel numeroMina = new JLabel(minas);
+			botonesJuego[i][j].removeAll();
+			panelJuego.add(numeroMina);
+		
+		}
+
+	}
+
+	/**
+	 * Muestra una ventana que indica el fin del juego
+	 * 
+	 * @param porExplosion
+	 *            : Un booleano que indica si es final del juego porque ha explotado
+	 *            una mina (true) o bien porque hemos desactivado todas (false)
+	 * @post : Todos los botones se desactivan excepto el de volver a iniciar el
+	 *       juego.
+	 */
+	public void mostrarFinJuego(boolean porExplosion) {
+		if (juego.esFinJuego()) {
+			JOptionPane.showInputDialog(null, "PUNTUCAI�N:" + juego.getPuntuacion(), "PUNTUACION FINAL");
+		} else {
+			JOptionPane.showInputDialog(null, "PUNTUCAI�N:" + juego.getPuntuacion(), "PUNTUACION FINAL");
+		}
+		// TODO
+	}
+
+	/**
+	 * Metodo que muestra la puntuacion por pantalla.
 	 */
 	public void actualizarPuntuacion() {
-		//TODO
+		panelPuntuacion.setName(String.valueOf(juego.getPuntuacion()));
 	}
-	
+
 	/**
-	 * Método para refrescar la pantalla
+	 * Metodo para refrescar la pantalla
 	 */
-	public void refrescarPantalla(){
-		ventana.revalidate(); 
+	public void refrescarPantalla() {
+		ventana.revalidate();
 		ventana.repaint();
 	}
 
 	/**
-	 * Método que devuelve el control del juego de una ventana
+	 * Metodo que devuelve el control del juego de una ventana
+	 * 
 	 * @return un ControlJuego con el control del juego de la ventana
 	 */
 	public ControlJuego getJuego() {
@@ -195,13 +223,12 @@ public class VentanaPrincipal {
 	/**
 	 * Método para inicializar el programa
 	 */
-	public void inicializar(){
-		//IMPORTANTE, PRIMERO HACEMOS LA VENTANA VISIBLE Y LUEGO INICIALIZAMOS LOS COMPONENTES.
+	public void inicializar() {
+		// IMPORTANTE, PRIMERO HACEMOS LA VENTANA VISIBLE Y LUEGO INICIALIZAMOS LOS
+		// COMPONENTES.
 		ventana.setVisible(true);
-		inicializarComponentes();	
-		inicializarListeners();		
+		inicializarComponentes();
+		inicializarListeners();
 	}
-
-
 
 }
